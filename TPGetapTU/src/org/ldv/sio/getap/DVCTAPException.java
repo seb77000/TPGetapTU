b5459084809adb897218c96db8d0fc502a935653
hchaudhry <chaudhry.hussam@gmail.com>
2012-10-11 15:06:55 +0200

    Début methodes (etat)

---
+++ TPGetapTU/src/org/ldv/sio/getap/DVCTAPException.java
@@ -0,0 +1,11 @@
+package org.ldv.sio.getap;
+
+public class DVCTAPException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DVCTAPException(String message) {
+		super(message);
+	}
+
+}
